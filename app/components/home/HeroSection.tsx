1a9420992416a56b0b65cb96d9c9d9718498499a
Chino <202111653@fit.edu.ph>
2026-02-10 01:42:06 +0800

    FEAT: Add logo image to HeroSection and remove placeholder text

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Section from "../ui/Section";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
     <Section className="w-full flex flex-col items-center justify-center">
       <div className="flex flex-col items-center gap-2">
-        <h1 className="text-4xl font-extrabold bg-linear-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent lg:text-6xl">
-          CS Expo 2026
-        </h1>
-        <p className="w-3/4 text-center text-purple-300 text-base lg:text-lg">
-          The Expo for Developers.
-        </p>
+        <Image
+          src="/Expo Design Theme.png"
+          alt="Logo"
+          width={400}
+          height={400}
+          className="bg-transparent"
+          style={{ backgroundColor: "transparent" }}
+        />
       </div>
     </Section>
   );
